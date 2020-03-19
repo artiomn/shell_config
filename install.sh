af6e2975baebfc603784326e3eb385af5e3cad27
Artiom N <artiom@artiom>
2020-03-20 01:25:44 +0300

    Oh-my-zsh cloning was added

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
 tar -C home -cf - . | tar -C "$HOME" -xf -
 if [ $(id -u) -eq 0 ]; then
   tar -C etc -cf - . | tar -C /etc -xf -
